d6d9010ddc4482aabea308bb71579fdd77585da7
arkadiuszrosinski <ar.rosinski@gmail.com>
2023-07-25 11:38:16 +0200

    DEV-1: RestApiController.php - add Interface

---
+++ src/Service/RestApiService.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Service;
+
+class RestApiService implements ApiServiceInterface
+{
+
+    public function addCompany()
+    {
+        // TODO: Implement addCompany() method.
+    }
+
+    public function addCompanyEmployee()
+    {
+        // TODO: Implement addCompanyEmployee() method.
+    }
+}
